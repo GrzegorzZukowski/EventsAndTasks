27d3102a36f58371a47b55a8e9b8304fc22c9258
grzegorzzukowskigda@gmail.com <grzegorzzukowskigda@gmail.com>
2019-10-17 09:03:46 +0200

    initial commit

---
+++ Ćwiczenie - zdarzenia i taski/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ćwiczenie___zdarzenia_i_taski
+{
+    //TODO: praca zespołowa - założyć repozytorium na github-ie
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOblicz_Click(object sender, EventArgs e)
+        {
+            Obliczenia obliczenia = new Obliczenia();
+            //TODO: zaimplementować obsługę zdarzeń "rozpoczecieObliczen", "progresObliczen", "zakonczenieObliczen"
+            //TODO: rozpoczecieObliczen ustawia progressBar na 0%
+            //TODO: zakonczenieObliczen ustawia progressBar na 100%
+            //TODO: progresObliczen ustawia progressBar na % taki jak wartość zmiennej wynik w atrybucie zdarzenia
+
+            //TODO: wykonać metodę Obliczaj() asynchronicznie (async - await) 
+            int wynik = obliczenia.Obliczaj();
+
+            MessageBox.Show($"wynik: {wynik}");
+        }
+    }
+}
---
+++ Ćwiczenie - zdarzenia i taski/Obliczenia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ćwiczenie___zdarzenia_i_taski
+{
+    public class Obliczenia
+    {
+        //TODO: klasa ma definiować zdarzenia "rozpoczecieObliczen", "progresObliczen", "zakonczenieObliczen"
+        //TODO: zdarzenie "progresObliczen" ma mieć atrybut "progresObliczen_EventArgs" który przechowuje aktualną wartość zmiennej 'wynik'
+
+        public int Obliczaj()
+        {
+            int wynik = -1;
+
+            //TODO: wywołanie zdarzenia "rozpoczecieObliczen" bez atrybutów zdarzenia
+
+            for (int i = 0; i <= 100; i++)
+            {
+                wynik++;
+                Thread.Sleep(100);
+                //TODO: wywołanie zdarzenia "progresObliczen" z atrybutem zdarzenia który zawierać ma aktualną wartość zmiennej 'wynik'
+            }
+
+            //TODO: wywołanie zdarzenia "zakonczenieObliczen" baz atrybutów zdarzenia
+
+            return wynik;
+        }
+    }
+}
